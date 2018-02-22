da7f30ac666b1daf9b3fe46afadb7128780a8a99
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2018-02-22 09:20:02 +0300

    Fix weird sorting of priority queue

--- Assignment-1/main.cpp
+++ Assignment-1/main.cpp
@@ -28,26 +28,20 @@ struct color_t {
     this->R = this->R * 0.1;
     this->R = this->R * 0.1;
   }
+  void print() const {
+    cout << "(" << this->R << ", " << this->G << ", " << this->B << ")" << endl;
+  }
 };
 
-struct threeDvec_t {
+struct position_t {
   int x;
   int y;
   int z;
-  threeDvec_t operator+(threeDvec_t other) {
-    return threeDvec_t { this->x + other.x, this->y + other.y, this->z + other.z };
+  position_t operator+(position_t other) {
+    return position_t { this->x + other.x, this->y + other.y, this->z + other.z };
   }
-  threeDvec_t operator-(threeDvec_t other) {
-    return threeDvec_t { this->x - other.x, this->y - other.y, this->z - other.z };
-  }
-  threeDvec_t operator*(double coeff) {
-    return threeDvec_t { (int) round(coeff) * this->x, (int) round(coeff) * this->y, (int) round(coeff) * this->z };
-  }
-  bool operator==(threeDvec_t other) {
-    return this->x == other.x && this->y == other.y && this->z == other.z;
-  }
-  int dot(threeDvec_t other) {
-    return this->x * other.x + this->y * other.y + this->z * other.z;
+  position_t operator-(position_t other) {
+    return position_t { this->x - other.x, this->y - other.y, this->z - other.z };
   }
   double length() {
     return sqrt(this->x * this->x + this->y * this->y + this->z * this->z); 
@@ -55,11 +49,68 @@ struct threeDvec_t {
   void print() const {
     cout << "(" << this->x << ", " << this->y << ", " << this->z << ")" << endl;
   }
+  bool operator==(position_t other) {
+    return this->x == other.x && this->y == other.y && this->z == other.z;
+  }
 };
+
+struct direction_t {
+  double x;
+  double y;
+  double z;
+  position_t approximate() {
+    return position_t { (int) round(this->x), (int) round(this->y), (int) round(this->z) };
+  }
+  double length() {
+    return sqrt(this->x * this->x + this->y * this->y + this->z * this->z); 
+  }
+  int dot(direction_t other) {
+    return this->x * other.x + this->y * other.y + this->z * other.z;
+  }
+  int dot(position_t other) {
+    return this->x * other.x + this->y * other.y + this->z * other.z;
+  }
+  direction_t operator*(double coeff) {
+    return direction_t { coeff * this->x, coeff * this->y, coeff * this->z };
+  }
+};
+
+direction_t pos_to_dir(position_t pos) {
+  return direction_t { (double) pos.x, (double) pos.y, (double) pos.z };
+}
+
+struct vector_t {
+  position_t origin;
+  direction_t direction;
+};
+
+/* struct threeDvec_t { */
+/*   int x; */
+/*   int y; */
+/*   int z; */
+/*   threeDvec_t operator+(threeDvec_t other) { */
+/*     return threeDvec_t { this->x + other.x, this->y + other.y, this->z + other.z }; */
+/*   } */
+/*   threeDvec_t operator-(threeDvec_t other) { */
+/*     return threeDvec_t { this->x - other.x, this->y - other.y, this->z - other.z }; */
+/*   } */
+/*   threeDvec_t operator*(double coeff) { */
+/*     return threeDvec_t { (int) round(coeff) * this->x, (int) round(coeff) * this->y, (int) round(coeff) * this->z }; */
+/*   } */
+/*   int dot(threeDvec_t other) { */
+/*     return this->x * other.x + this->y * other.y + this->z * other.z; */
+/*   } */
+/*   double length() { */
+/*     return sqrt(this->x * this->x + this->y * this->y + this->z * this->z); */ 
+/*   } */
+/*   void print() const { */
+/*     cout << "(" << this->x << ", " << this->y << ", " << this->z << ")" << endl; */
+/*   } */
+/* }; */
 
 struct Sphere {
   color_t color;
-  threeDvec_t center;
+  position_t center;
   int radius;
 };
 
@@ -74,13 +125,13 @@ color_t** init_plane() {
   return plane;
 }
 
-struct ray_vec_t {
-  threeDvec_t origin;
-  threeDvec_t direction;
-  void print() {
-    cout << origin.x << "x + " << origin.y << "y + " << origin.z << " + t(" << direction.x << "x + " << direction.y << "y + " << direction.z << "z)" << endl;
-  }
-};
+/* struct ray_vec_t { */
+/*   threeDvec_t origin; */
+/*   threeDvec_t direction; */
+/*   void print() { */
+/*     cout << origin.x << "x + " << origin.y << "y + " << origin.z << " + t(" << direction.x << "x + " << direction.y << "y + " << direction.z << "z)" << endl; */
+/*   } */
+/* }; */
 
 template <typename action>
 void forall_plane(color_t** plane, action act) {
@@ -105,67 +156,61 @@ void color_annot(string color_component, int sphere_number) {
 
 struct intersection_t {
   color_t color;
-  threeDvec_t point;
+  position_t point;
 };
 
-auto create_comparator(threeDvec_t vec) {
+auto create_comparator(position_t vec) {
   return [&vec](intersection_t l, intersection_t r) {
     return (l.point - vec).length() > (r.point - vec).length(); 
   };
 }
 
-auto ray_sphere_intersection(
-    ray_vec_t ray_vec,
-    vector<Sphere> spheres
-) {
-  auto comparator = create_comparator(ray_vec.origin);
-  priority_queue<
-    intersection_t,
-    vector<intersection_t>,
-    decltype(comparator)
-  > *intersections = new priority_queue<intersection_t, vector<intersection_t>, decltype(comparator)>(comparator);
+vector<intersection_t> *ray_sphere_intersection(vector_t ray_vec, vector<Sphere> spheres) {
+  vector<intersection_t> *intersections = new vector<intersection_t>();
   for(vector<Sphere>::iterator it = spheres.begin(); it != spheres.end(); it++) {
-    cout << "Sphere " << endl << "Pos: ";
-    it->center.print();
-    cout << "Radius: " << it->radius << endl;
-    ray_vec.print();
-    cout << endl << endl;
+    /* cout << "Sphere " << endl << "Pos: "; */
+    /* it->center.print(); */
+    /* cout << "Radius: " << it->radius << endl; */
+    /* cout << endl << endl; */
     Sphere sphere = *it;
-    double A = pow(ray_vec.direction.length(), 2);
+    double A = ray_vec.direction.dot(ray_vec.direction);
     double B = 2 * (ray_vec.direction.dot(ray_vec.origin - sphere.center));
-    double C = pow((ray_vec.origin - sphere.center).length(), 2) - sphere.radius * sphere.radius;
-    double delta = B * B - 4 * A * C;
-    if(DEBUG) cout << "Delta: " << delta << endl;
-    if(delta < 0) {
+    double C = pow((ray_vec.origin - sphere.center).length(), 2) - pow(sphere.radius, 2);
+    double discr = B * B - 4 * A * C;
+    if(DEBUG) cout << "Delta: " << discr << endl;
+    if(discr < 0) {
       // No intersection
       if(DEBUG) cout << "No intersections!" << endl;
-    } else if (delta == 0) {
+    } else if (discr == 0) {
       // One intersection
       double t = -B / (2 * A);
       if(DEBUG) cout << "One intersection: " << t << endl;
-      threeDvec_t point = ray_vec.origin + ray_vec.direction * t;
-      intersections->push(intersection_t { sphere.color, point });
+      position_t point = ray_vec.origin + (ray_vec.direction * t).approximate();
+      intersections->push_back(intersection_t { sphere.color, point });
     } else {
       // Two intersections
-      double t1 = (-B - sqrt(delta)) / (2 * A);
-      double t2 = (-B + sqrt(delta)) / (2 * A);
+      double t1 = (-B - sqrt(discr)) / (2 * A);
+      double t2 = (-B + sqrt(discr)) / (2 * A);
       if(DEBUG) cout << "Two intersections: " << t1 << " and " << t2 << endl;
-      threeDvec_t point1 = ray_vec.origin + ray_vec.direction * t1;
-      threeDvec_t point2 = ray_vec.origin + ray_vec.direction * t2;
+      position_t point1 = ray_vec.origin + (ray_vec.direction * t1).approximate();
+      position_t point2 = ray_vec.origin + (ray_vec.direction * t2).approximate();
       if(DEBUG) cout << "Point 1:";
       point1.print();
       if(DEBUG) cout << "Point 2:";
       point2.print();
-      intersections->push(intersection_t { sphere.color, point1 });
-      intersections->push(intersection_t { sphere.color, point2 });
+      intersections->push_back(intersection_t { sphere.color, point1 });
+      intersections->push_back(intersection_t { sphere.color, point2 });
     }
     if(DEBUG) cout << "-----------------------------------------------" << endl;
   }
+  sort(intersections->begin(), intersections->end(), [&ray_vec](intersection_t l, intersection_t r) {
+    return (l.point - ray_vec.origin).length() > (r.point - ray_vec.origin).length(); 
+  });
   return intersections;
 }
 
 color_t white_color = color_t { 255, 255, 255 };
-threeDvec_t light_pos = threeDvec_t LIGHT_VEC;
+position_t light_pos = position_t LIGHT_VEC;
 
 void shadow_point(intersection_t* focus_intersection, vector<Sphere> spheres) {
   cout << "-- Shadowing" << endl;
@@ -174,20 +219,20 @@ void shadow_point(intersection_t* focus_intersection, vector<Sphere> spheres) {
   cout << "Vector: ";
   (light_pos - focus_intersection->point).print();
   cout << "-----------------------------------------------" << endl;
-  ray_vec_t shadow_vec = ray_vec_t { focus_intersection->point, light_pos - focus_intersection->point };
-  auto *intersections = ray_sphere_intersection(shadow_vec, spheres);
+  vector_t shadow_vec = vector_t { focus_intersection->point, pos_to_dir(light_pos - focus_intersection->point) };
+  vector<intersection_t> *intersections = ray_sphere_intersection(shadow_vec, spheres);
   if(intersections->empty()) {
     // This intersection point is taking perfect direct light.
     return;
   } else {
     cout << "wow";
     cout << intersections->size();
-    intersection_t elem = intersections->top();
+    intersection_t elem = intersections->back();
     while(elem.point == focus_intersection->point && !intersections->empty()) {
-      intersections->pop();
+      intersections->pop_back();
       cout << "wow" << endl;
       cout << intersections->size() << endl;
-      intersection_t elem = intersections->top();
+      intersection_t elem = intersections->back();
     }
     if(!intersections->empty()) {
       focus_intersection->color.desaturate();
@@ -202,21 +247,28 @@ void shadow_point(intersection_t* focus_intersection, vector<Sphere> spheres) {
   cout << endl;
 }
 
-color_t shoot_ray(ray_vec_t ray_vec, vector<Sphere> spheres) {
-  auto *intersections = ray_sphere_intersection(ray_vec, spheres);
+color_t shoot_ray(vector_t ray_vec, vector<Sphere> spheres) {
+  vector<intersection_t> *intersections = ray_sphere_intersection(ray_vec, spheres);
   if(intersections->empty()) {
     return white_color;
   } else {
-    intersection_t elem = intersections->top();
+    intersection_t elem = intersections->back();
 
-    threeDvec_t origin = threeDvec_t { 0, 0, 0 }; // TODO: Fix, duplication
+    position_t origin = position_t { 0, 0, 0 }; // TODO: Fix, duplication
     while(elem.point == origin && !intersections->empty()) {
-      intersections->pop();
-      intersection_t elem = intersections->top();
+      cout << "Clearing one (0, 0, 0)" << endl;
+      intersections->pop_back();
+      intersection_t elem = intersections->back();
     }
     if(intersections->empty()) return white_color;
-    intersection_t closest_intersection = intersections->top();
-    shadow_point(&closest_intersection, spheres);
+    intersection_t closest_intersection = intersections->back();
+    cout << "All intersections:" << endl;
+    while(!intersections->empty()) {
+      intersections->back().point.print();
+      intersections->back().color.print();
+      intersections->pop_back();
+    }
+    /* shadow_point(&closest_intersection, spheres); */
     return closest_intersection.color;
   }
 }
@@ -247,12 +299,13 @@ int main()
     /*   int radius; */
     /*   sphere_annot("Radius", i); */
     /*   cin >> radius; */
-    /*   spheres->push_back(Sphere { color_t { R, G, B }, threeDvec_t { x, y, z }, radius }); */
+    /*   spheres->push_back(Sphere { color_t { R, G, B }, position_t { x, y, z }, radius }); */
     /* }; */
-    spheres->push_back(Sphere { color_t { 255, 0, 0 }, threeDvec_t { 0, 0, 600 }, 100 });
+    spheres->push_back(Sphere { color_t { 255, 0, 0 }, position_t { 0, 0, 200 }, 40 });
+    spheres->push_back(Sphere { color_t { 0, 0, 255 }, position_t { 50, 0, 600 }, 100 });
     color_t **empty_plane = init_plane();
-    threeDvec_t origin = threeDvec_t { 0, 0, 0 };
-    forall_plane(empty_plane, [origin, spheres](int x, int y){ return shoot_ray(ray_vec_t { origin, threeDvec_t { x, y, PLANE_Z } }, *spheres); });
+    position_t origin = position_t { 0, 0, 0 };
+    forall_plane(empty_plane, [origin, spheres](int x, int y){ return shoot_ray(vector_t { origin, pos_to_dir(position_t { x, y, PLANE_Z }) }, *spheres); });
 
     /* Drawing the image */
     bitmap_image image(100, 100);
